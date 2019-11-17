85947fe8140b2b8cc2739b3c2bf2e089b94c20de
Alexander Williams <williams_alex@protonmail.com>
2019-11-17 16:00:58 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,15 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(library)
+  file = File.read(library)
+  emoticons = YAML.load(file)
+  puts emoticons
+  emoticons['get_meaning'] = {emoticons.each{|item| {item[1], item[0]}}}
+  emoticons['get_emoticon'] = {emoticons.each{|item| item[]}
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(emoticon)
+  
 end
 
 def get_english_meaning
